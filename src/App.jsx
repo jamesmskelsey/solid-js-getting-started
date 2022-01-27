6d89f444080504b9eeb9b54ffca4fce466ac4b4d
James Kelsey <jamesmskelsey@gmail.com>
2022-01-27 09:36:48 -0800

    memoized helps calculate a value only once per RENDER

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 import logo from "./logo.svg";
 import styles from "./App.module.css";
-import { createSignal, createEffect } from "solid-js";
+import { createSignal, createEffect, createMemo } from "solid-js";
 
 function App() {
   const [first, setFirst] = createSignal("JSON");
   const [last, setLast] = createSignal("Bourne");
 
   const capitalName = (name) => name.toUpperCase();
+
+  const nameValue = (name) => {
+    console.log("Calculating value of", first());
+    return name.split("").reduce((total, letter) => {
+      return total + letter.charCodeAt(0);
+    }, 0);
+  };
+
+  const nameValueMemoized = createMemo(() => {
+    // return the value of the character codes of the name added up
+    return nameValue(capitalName(first()));
+  });
 
   return (
     <div class={styles.App}>
@@ -25,7 +37,8 @@ function App() {
           />
         </div>
         <p>
-          Hello, {capitalName(first())} {capitalName(last())}
+          {capitalName(first())} is worth {nameValueMemoized(first())}{" "}
+          {nameValueMemoized(first())} {nameValueMemoized(first())}
         </p>
       </header>
     </div>
