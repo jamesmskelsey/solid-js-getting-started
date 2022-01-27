e33f0109045d15e30e4d071051a1bc65a778a0f9
James Kelsey <jamesmskelsey@gmail.com>
2022-01-27 11:05:25 -0800

    playing with For and Index

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 import logo from "./logo.svg";
 import styles from "./App.module.css";
-import { createSignal, createEffect, createMemo } from "solid-js";
+import {
+  createSignal,
+  createEffect,
+  createMemo,
+  Show,
+  For,
+  Index,
+} from "solid-js";
 
 function App() {
   const [first, setFirst] = createSignal("JSON");
   const [last, setLast] = createSignal("Bourne");
+  const [loggedIn, setLoggedIn] = createSignal(false);
+  const [names, setNames] = createSignal([
+    { id: 1, name: "James" },
+    { id: 2, name: "Ayako" },
+    { id: 3, name: "Aiden" },
+    { id: 4, name: "Mei" },
+  ]);
 
   const capitalName = (name) => name.toUpperCase();
 
@@ -15,19 +29,37 @@ function App() {
     }, 0);
   };
 
+  // testing memoization. took it out of the app to play with something else
   const nameValueMemoized = createMemo(() => {
     // return the value of the character codes of the name added up
     return nameValue(capitalName(first()));
   });
 
+  // Yep. This toggles loggedin. Yep, that Show component only renders a logged in with no
+  // way to log out. Point of the app is to play an experiment... not do anything useful.
+  const toggle = () => {
+    setLoggedIn(!loggedIn());
+  };
+
+  const validName = () => {
+    return names()
+      .reduce((a, b) => {
+        return [...a, b.name.toLowerCase()];
+      }, [])
+      .includes(first().toLowerCase());
+  };
+
   return (
     <div class={styles.App}>
       <header class={styles.header}>
         <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
+      </header>
+      <main>
         <div>
+          <label for="first_name">
+            When your entry is of high enough value...
+          </label>
+          <br></br>
           <input
             name="first_name"
             value={first()}
@@ -37,10 +69,29 @@ function App() {
           />
         </div>
         <p>
-          {capitalName(first())} is worth {nameValueMemoized(first())}{" "}
-          {nameValueMemoized(first())} {nameValueMemoized(first())}
+          {capitalName(first())} is{" "}
+          {validName() ? "a valid name" : "an invalid name"}
         </p>
-      </header>
+        <Show when={validName() && !loggedIn()}>
+          <button onClick={toggle}>Login</button>
+        </Show>
+        <p>Using a For component.</p>
+        <For each={names()}>
+          {(name, i) => (
+            <p>
+              {i() + 1} {name.name}
+            </p>
+          )}
+        </For>
+        <p>Using an Index component.</p>
+        <Index each={names()}>
+          {(name, i) => (
+            <p>
+              {i + 1} {name().name}
+            </p>
+          )}
+        </Index>
+      </main>
     </div>
   );
 }
